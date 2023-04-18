d54a60bca53454c97a9aaabb0ee47408acf2b8a8
kk <gitaragra@gmail.com>
2023-04-18 16:43:32 +0200

    Add TableHead component

--- laboratory_lab/src/pages/tableReactions/_indexTableReactions/IndexTable.tsx
+++ laboratory_lab/src/pages/tableReactions/_indexTableReactions/IndexTable.tsx
@@ -1,89 +1,22 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from "react-redux";
-import { useValidationForm } from "../../../hooks/useValidationForm";
-import { RootState } from "../../../redux/store";
-import { handleTableOpen } from "../../../redux/storeFeatures/tableReactionsSlice";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../../data/firebaseConfig";
 import TableBody from "../tableBody/TableBody";
 import TableHead from "../tableHead/TableHead";
+import TableHeader from "../tableHeader/TableHeader";
 import TableReactions from "../tableReactions/TableReactions";
-import { auth } from "../../../data/firebaseConfig";
-import { useNavigate } from "react-router-dom";
 
 const IndexTable = () => {
-  
- const navigate = useNavigate();
- useEffect(() => {
-   auth.onAuthStateChanged(user => {
-     !user && navigate("/login");
-   });
- }, []);
-
-  const dispatch = useDispatch();
-  const { printReactions, editedReaction } = useSelector(
-    (state: RootState) => state.tableReactions
-  );
-
-  const { isOpen } = useSelector(
-    (state: RootState) => state.tableReactions.toggleTable
-  );
-
-  const { validationForm } = useValidationForm(editedReaction);
-
-  const conditionsPrint =
-    !editedReaction.isEdit &&
-    (validationForm().name ||
-      validationForm().technics ||
-      validationForm().alcaloids ||
-      validationForm().selectMilimolles ||
-      validationForm().substract ||
-      validationForm().selectReactionCondition ||
-      validationForm().solvents)
-      ? { color: "red", text: "Uzupełnij" }
-      : { color: "black", text: "Parametry" };
-
-  const timePrint =
-    !editedReaction.isEdit &&
-    (validationForm().startDate ||
-      validationForm().finishDate ||
-      validationForm().startTime ||
-      validationForm().finishTime)
-      ? { color: "red", text: "Uzupełnij" }
-      : { color: "black", text: "Czasy" };
+  const navigate = useNavigate();
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      !user && navigate("/login");
+    });
+  }, []);
 
   return (
     <main className="tableReactions__main">
-      <section className="wrapper tableReactions__headers">
-        <div
-          onClick={() => dispatch(handleTableOpen(true))}
-          style={{
-            color: conditionsPrint.color,
-            backgroundColor: `${
-              isOpen ? "rgb(142,151,164)" : "rgb(255 255 255)"
-            }`,
-          }}
-          className="tableReactions__header"
-        >
-          {conditionsPrint.text}
-        </div>
-        <div
-          onClick={() => dispatch(handleTableOpen(false))}
-          style={{
-            color: timePrint.color,
-            backgroundColor: `${
-              isOpen ? "rgb(255 255 255)" : "rgb(142,151,164)"
-            }`,
-          }}
-          className="tableReactions__header"
-        >
-          {timePrint.text}
-        </div>
-        <div className="tableReactions__header  tableReactions__header--counter ">
-          {printReactions.length > 0
-            ? `Liczba reakcji: ${printReactions.length}`
-            : ""}
-        </div>
-      </section>
-
+      <TableHeader />
       <TableReactions>
         <TableHead />
         <TableBody />
---
+++ laboratory_lab/src/pages/tableReactions/tableHeader/TableHeader.tsx
@@ -0,0 +1,75 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
+import { useValidationForm } from "../../../hooks/useValidationForm";
+import { handleTableOpen } from "../../../redux/storeFeatures/tableReactionsSlice";
+
+const TableHeader = () => {
+  const dispatch = useDispatch();
+
+  const { printReactions, editedReaction } = useSelector(
+    (state: RootState) => state.tableReactions
+  );
+
+  const { isOpen } = useSelector(
+    (state: RootState) => state.tableReactions.toggleTable
+  );
+
+  const { validationForm } = useValidationForm(editedReaction);
+
+  const conditionsPrint =
+    !editedReaction.isEdit &&
+    (validationForm().name ||
+      validationForm().technics ||
+      validationForm().alcaloids ||
+      validationForm().selectMilimolles ||
+      validationForm().substract ||
+      validationForm().selectReactionCondition ||
+      validationForm().solvents)
+      ? { color: "red", text: "Uzupełnij" }
+      : { color: "black", text: "Parametry" };
+
+  const timePrint =
+    !editedReaction.isEdit &&
+    (validationForm().startDate ||
+      validationForm().finishDate ||
+      validationForm().startTime ||
+      validationForm().finishTime)
+      ? { color: "red", text: "Uzupełnij" }
+      : { color: "black", text: "Czasy" };
+
+  return (
+    <section className="wrapper tableReactions__headers">
+      <div
+        onClick={() => dispatch(handleTableOpen(true))}
+        style={{
+          color: conditionsPrint.color,
+          backgroundColor: `${
+            isOpen ? "rgb(142,151,164)" : "rgb(255 255 255)"
+          }`,
+        }}
+        className="tableReactions__header"
+      >
+        {conditionsPrint.text}
+      </div>
+      <div
+        onClick={() => dispatch(handleTableOpen(false))}
+        style={{
+          color: timePrint.color,
+          backgroundColor: `${
+            isOpen ? "rgb(255 255 255)" : "rgb(142,151,164)"
+          }`,
+        }}
+        className="tableReactions__header"
+      >
+        {timePrint.text}
+      </div>
+      <div className="tableReactions__header  tableReactions__header--counter ">
+        {printReactions.length > 0
+          ? `Liczba reakcji: ${printReactions.length}`
+          : ""}
+      </div>
+    </section>
+  );
+};
+
+export default TableHeader;
